0ef3aded02c43614471e2b1a7671c82240ad40d5
Savannah Dautel <sadautel@gmail.com>
2020-03-14 15:57:27 -0600

    testing

--- create.sql
+++ create.sql
@@ -12,21 +12,26 @@ CREATE TABLE person
 CREATE TABLE login
 (
 	id SERIAL NOT NULL PRIMARY KEY,
-	username VARCHAR(50) NOT NULL UNIQUE,
+	username VARCHAR(50) NOT NULL ,
 	password VARCHAR(50) NOT NULL
 );
 
 
 CREATE TABLE add_recipes
 ( id SERIAL NOT NULL PRIMARY KEY
-, account_user INT NOT NULL REFERENCES create_account(id)
+, account_user INT NOT NULL REFERENCES person(id)
 , recipe_name VARCHAR(50) NOT NULL
 , ingredients VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE comments
-(allergy_id INT NOT NULL REFERENCES create_account(id)
+CREATE TABLE allergy
+(allergy_id INT NOT NULL REFERENCES person(id)
 , allergen VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE comments
+(user_id INT NOT NULL REFERENCES person(id)
+, comments VARCHAR(100) NOT NULL
 );
 
 INSERT INTO person(firstName, lastName, phone, email) VALUES
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var app = express();
-var getSignUp = require('getSignUp.js');
+var getSignUp = require('./getSignUp.js');
 const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL || "postgres://futdxhkvnuuepy:f187d7af0a259f26677add7247aced689bd76f3d8be42fc721d4f0e38c3bd367@ec2-52-45-14-227.compute-1.amazonaws.com:5432/d137sulfnslg99/recipeDb";
