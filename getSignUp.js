8bb7348660da596fcf8ec851bc8a229229def452
Savannah Dautel <sadautel@gmail.com>
2020-03-18 14:16:41 -0600

    testing

--- getSignUp.js
+++ getSignUp.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL || "postgres://futdxhkvnuuepy:f187d7af0a259f26677add7247aced689bd76f3d8be42fc721d4f0e38c3bd367@ec2-52-45-14-227.compute-1.amazonaws.com:5432/d137sulfnslg99/recipeDb";
 const pool = new Pool({connectionString: connectionString});
 
-function getSignIn(req, res) {
+function getSignUp(req, res) {
    console.log("Sending info to another file");
 
    var firstName = req.query.firstName;
