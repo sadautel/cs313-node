e7dc75dcb569195d81166fad24112a957fd930fe
Savannah Dautel <sadautel@gmail.com>
2020-04-02 19:41:07 -0600

    test

--- create.sql
+++ create.sql
@@ -5,9 +5,8 @@ CREATE TABLE person
 	id SERIAL NOT NULL PRIMARY KEY,
 	firstName VARCHAR(50) NOT NULL,
 	lastName  VARCHAR(50) NOT NULL,
-	phone     VARCHAR(100) NOT NULL,
-	email     VARCHAR(100) NOT NULL,
-	password     VARCHAR(100) NOT NULL
+	username VARCHAR(32) NOT NULL,
+    password_hash VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE login
@@ -22,9 +21,10 @@ CREATE TABLE add_recipes
 ( id SERIAL NOT NULL PRIMARY KEY
 , recipe_name VARCHAR(50) NOT NULL
 , recipe_description VARCHAR(100) NOT NULL
+, image_url TEXT
 , prep_time VARCHAR(100) NOT NULL
 , cook_time VARCHAR(100) NOT NULL
-, ingredients VARCHAR(100) NOT NULL
+,  ingredient TEXT [] NOT NULL
 );
 
 CREATE TABLE allergy
@@ -37,9 +37,3 @@ CREATE TABLE comments
 , comments VARCHAR(100) NOT NULL
 );
 
-INSERT INTO person(firstName, lastName, phone, email) VALUES
-  ('Savannah'
-  , 'Dautel'
-  , '801-530-9905'
-  ,'sadautel@gmail.com'
-  );
